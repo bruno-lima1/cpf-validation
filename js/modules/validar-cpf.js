011855caa7c2b976eb42cf59f279881974d15939
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-25 15:43:26 -0300

    cpf validation review

--- js/modules/validar-cpf.js
+++ js/modules/validar-cpf.js
@@ -1,57 +1,55 @@
 export default class ValidarCpf {
   constructor(cpf) {
     this.cpf = document.getElementById(cpf);
-  }
-  clear(cpf) {
-    return cpf.replace(/\D/gi, "");
-  }
-  build(cpf) {
-    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4");
-  }
-  format(cpf) {
-    return this.build(this.clear(cpf));
+    this.returnCPF = this.returnCPF.bind(this)
+    this.preventDefault = this.preventDefault.bind(this)
   }
   init() {
     if (this.cpf) {
       this.addEvent();
-      this.addErrorSpan();
-      // console.log(this.clear(this.cpf))
+      // this.addErrorSpan();
     }
     return this;
   }
   addEvent() {
-    this.cpf.addEventListener("change", () => {
-      return this.returnCPF(this.cpf);
-    });
-    this.cpf.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-      }
-    });
+    this.cpf.addEventListener("keydown", this.preventDefault)
+    this.cpf.addEventListener("change", this.returnCPF)
   }
-  validation(cpf) {
-    const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/gi);
-    return matchCpf && matchCpf[0] === cpf;
+  clear() {
+    return this.cpf.value.replace(/\D/gi, "");
   }
-  returnCPF(cpf) {
-    if (this.validation(cpf.value)) {
-      cpf.value = this.format(cpf.value);
-      cpf.classList.add("valid");
-      cpf.classList.remove("error");
-      cpf.nextElementSibling.classList.remove("active");
-    } else {
-      cpf.classList.add("error");
-      cpf.classList.remove("valid");
-      cpf.nextElementSibling.classList.add("active");
+  build() {
+    return this.cpf.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4");
+  }
+  preventDefault(event) {
+    if (event.key === "Enter") {
+      event.preventDefault()
     }
   }
-  addErrorSpan() {
-    const errorElement = document.createElement("span");
-    errorElement.classList.add("error-text");
-    errorElement.innerText = "CPF Inválido";
-    this.cpf.parentElement.insertBefore(
-      errorElement,
-      this.cpf.nextElementSibling
-    );
+  returnCPF() {
+    this.cpf.value = this.build(this.clear(this.cpf.value))
+    // if (this.validation(cpf.value)) {
+    //   cpf.value = this.format(cpf.value);
+    //   cpf.classList.add("valid");
+    //   cpf.classList.remove("error");
+    //   // cpf.nextElementSibling.classList.remove("active");
+    // } else {
+    //   cpf.classList.add("error");
+    //   cpf.classList.remove("valid");
+    //   // cpf.nextElementSibling.classList.add("active");
+    // }
   }
+  // validation(cpf) {
+  //   const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/gi);
+  //   return matchCpf && matchCpf[0] === cpf;
+  // }
+  // addErrorSpan() {
+  //   const errorElement = document.createElement("span");
+  //   errorElement.classList.add("error-text");
+  //   errorElement.innerText = "CPF Inválido";
+  //   this.cpf.parentElement.insertBefore(
+  //     errorElement,
+  //     this.cpf.nextElementSibling
+  //   );
+  // }
 }
