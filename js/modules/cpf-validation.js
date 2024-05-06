b006d78ef65bf465e3544b34c1be64192e741d59
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-06 15:34:58 -0300

    updating js

--- js/modules/cpf-validation.js
+++ js/modules/cpf-validation.js
@@ -1,4 +1,50 @@
-
+export default class CPFValidation {
+  constructor(cpf) {
+    this.cpf = document.getElementById(cpf);
+    this.preventDefault = this.preventDefault.bind(this)
+    this.enableValidation = this.enableValidation.bind(this)
+  }
+  init() {
+    if (this.cpf) {
+      this.addEvent()
+      this.errorMessage()
+    }
+    return this;
+  }
+  addEvent() {
+    this.cpf.addEventListener("keydown", this.preventDefault)
+    this.cpf.addEventListener("change", this.enableValidation)
+  }
+  preventDefault(event) {
+    if (event.key === "Enter") {
+      event.preventDefault()
+    }
+  }
+  clear() {
+    return this.cpf.value.replace(/\D/gi, "")
+  }
+  build() {
+    return this.cpf.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4")
+  }
+  format() {
+    return this.build(this.clear(this.cpf.value))
+  }
+  validation() {
+    const matchCPF = this.cpf.value.match(/(\d{3}[.-]?){3}\d{2}/gi)
+    return matchCPF && matchCPF[0] === this.cpf.value
+  }
+  enableValidation() {
+    return this.validation()
+      ? (this.cpf.value = this.format(this.cpf.value), this.cpf.classList.add("valid"), this.cpf.classList.remove("error"), this.cpf.nextElementSibling.classList.remove("active"))
+      : (this.cpf.classList.add("error"), this.cpf.classList.remove("valid"), this.cpf.nextElementSibling.classList.add("active"))
+  }
+  errorMessage() {
+    const span = document.createElement("span");
+    span.classList.add("error-text");
+    span.innerHTML = "CPF Inválido";
+    this.cpf.parentElement.insertBefore(span, this.cpf.nextElementSibling)
+  }
+}
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-
+import CPFValidation from "./modules/cpf-validation.js";
+const cpfValidation = new CPFValidation("cpf");
+cpfValidation.init()
 
 
 
