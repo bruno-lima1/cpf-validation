1b94f74ac8685a5c191a2f9e2d5b4b2af135d0cf
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-14 09:42:08 -0300

    cpf validation review

--- js/modules/cpf-validation.js
+++ js/modules/cpf-validation.js
@@ -1,6 +1,6 @@
 export default class CPFValidation {
   constructor(cpf) {
-    this.cpf = document.getElementById(cpf);
+    this.cpf = document.getElementById(cpf)
     this.preventDefault = this.preventDefault.bind(this)
     this.enableValidation = this.enableValidation.bind(this)
   }
@@ -20,117 +20,25 @@ export default class CPFValidation {
       event.preventDefault()
     }
   }
+  enableValidation() {
+    return this.validation()
+    ? (this.cpf.value = this.build(this.clear(this.cpf.value)), this.cpf.classList.add("valid"), this.cpf.classList.remove("error"), this.cpf.nextElementSibling.classList.remove("active"))
+    : (this.cpf.classList.add("error"), this.cpf.classList.remove("valid"), this.cpf.nextElementSibling.classList.add("active"))
+  }
+  validation() {
+    const matchCPF = this.cpf.value.match(/(\d{3}[.-]?){3}\d{2}/gi)
+    return matchCPF && matchCPF[0] === this.cpf.value
+  }
   clear() {
     return this.cpf.value.replace(/\D/gi, "")
   }
   build() {
     return this.cpf.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4")
   }
-  format() {
-    return this.build(this.clear(this.cpf.value))
-  }
-  validation() {
-    const matchCPF = this.cpf.value.match(/(\d{3}[.-]?){3}\d{2}/gi)
-    return matchCPF && matchCPF[0] === this.cpf.value
-  }
-  enableValidation() {
-    return this.validation()
-      ? (this.cpf.value = this.format(this.cpf.value), this.cpf.classList.add("valid"), this.cpf.classList.remove("error"), this.cpf.nextElementSibling.classList.remove("active"))
-      : (this.cpf.classList.add("error"), this.cpf.classList.remove("valid"), this.cpf.nextElementSibling.classList.add("active"))
-  }
   errorMessage() {
     const span = document.createElement("span");
-    span.classList.add("error-text");
+    span.classList.add("error-text")
     span.innerHTML = "CPF Inválido";
     this.cpf.parentElement.insertBefore(span, this.cpf.nextElementSibling)
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default class CpfValidation {
-//   constructor(cpf) {
-//     this.cpf = document.getElementById(cpf)
-//     this.returnCpf = this.returnCpf.bind(this)
-//   }
-//   init() {
-//     if (this.cpf) {
-//       this.addEvent()
-//       this.errorMessage()
-//     }
-//     return this;
-//   }
-//   addEvent() {
-//     this.cpf.addEventListener("keydown", this.preventDefault)
-//     this.cpf.addEventListener("change", this.returnCpf)
-//   }
-//   preventDefault(event) {
-//     if (event.key === "Enter") {
-//       event.preventDefault()
-//     }
-//   }
-//   validation() {
-//     const matchCpf = this.cpf.value.match(/(\d{3}[.-]?){3}\d{2}/gi)
-//     return matchCpf && matchCpf[0] === this.cpf.value
-//   }
-//   clear() {
-//     return this.cpf.value.replace(/\D/gi, "")
-//   }
-//   build() {
-//     return this.cpf.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4")
-//   }
-//   format() {
-//     return this.build(this.clear(this.cpf.value))
-//   }
-//   returnCpf() {
-//     return this.validation()
-//       ? (this.cpf.value = this.format(this.cpf.value), this.cpf.classList.add("valid"), this.cpf.classList.remove("error"), this.cpf.nextElementSibling.classList.remove("active"))
-//       : (this.cpf.classList.add("error"), this.cpf.classList.remove("valid"), this.cpf.nextElementSibling.classList.add("active"))
-//   }
-//   errorMessage() {
-//     const span = document.createElement("span");
-//     span.classList.add("error-text");
-//     span.innerHTML = "CPF inválido";
-//     this.cpf.parentElement.insertBefore(span, this.cpf.nextElementSibling)
-//   }
-// }
